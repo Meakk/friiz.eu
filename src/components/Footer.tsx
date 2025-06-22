a0be68705ce8045be16f0580c0bb4039e11e2a57
Michael Migliore <mcmigliore@gmail.com>
2025-06-22 21:37:19 +0200

    remove phone

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,6 @@ const Footer = () => {
       <p>
         <span style={{ display: "inline-flex", alignItems: "center" }}><FaEnvelope style={{ marginRight: "0.5rem" }} /><a href="mailto:contact@friiz.eu">contact@friiz.eu</a></span>
         <span style={{ margin: "0 0.5rem" }}>|</span>
-        <span style={{ display: "inline-flex", alignItems: "center" }}><FaPhoneAlt style={{ marginRight: "0.5rem" }} /><a href="tel:+33603783967">+33 6 03 78 39 67</a></span>
-        <span style={{ margin: "0 0.5rem" }}>|</span>
         <span style={{ display: "inline-flex", alignItems: "center" }}><FaMapMarkerAlt style={{ marginRight: "0.5rem" }} />Lyon, France</span>
       </p>
       <p>Copyright &copy; {new Date().getFullYear()} Friiz.eu</p>
